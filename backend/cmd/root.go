9dd1f6b21877ee20c8691be7ea2e72d8f19e6755
Nicky Semenza <nicky@nickysemenza.com>
2018-06-03 23:39:22 -0700

    add cobra for CLI

---
+++ backend/cmd/root.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nickysemenza/hyperion/backend/cue"
+	"github.com/nickysemenza/hyperion/backend/hyperion"
+	"github.com/nickysemenza/hyperion/backend/light"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "hyperion",
+	Short: "Hyperion is hype",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+var cmdServer = &cobra.Command{
+	Use:   "server",
+	Short: "Run the server",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Running Server")
+		light.ReadLightConfigFromFile("./light/testconfig.json")
+
+		go func() {
+			c, _ := cue.BuildCueFromCommand("hue1:#00FF00:1000")
+			cs := cue.GetCueMaster().GetDefaultCueStack()
+			cs.EnQueueCue(*c)
+		}()
+
+		hyperion.RunServer()
+	},
+}
+
+var cmdClient = &cobra.Command{
+	Use:   "client",
+	Short: "Run the client",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("TODO: client")
+	},
+}
+
+//Execute gives control to cobra
+func Execute() {
+
+	rootCmd.AddCommand(cmdServer)
+	rootCmd.AddCommand(cmdClient)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"github.com/nickysemenza/hyperion/backend/cue"
-	"github.com/nickysemenza/hyperion/backend/hyperion"
-	"github.com/nickysemenza/hyperion/backend/light"
-)
+import "github.com/nickysemenza/hyperion/backend/cmd"
 
 func main() {
-	light.ReadLightConfigFromFile("./light/testconfig.json")
-
-	go func() {
-		c, _ := cue.BuildCueFromCommand("hue1:#00FF00:1000")
-		cs := cue.GetCueMaster().GetDefaultCueStack()
-		cs.EnQueueCue(*c)
-	}()
-
-	hyperion.RunServer()
+	cmd.Execute()
 }
