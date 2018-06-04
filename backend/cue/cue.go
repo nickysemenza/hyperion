23a8d5e241466ae63d756288e06f97d6aafb2f94
Nicky Semenza <nicky@nickysemenza.com>
2018-06-03 18:53:32 -0700

    move cuemaster things into new file

--- backend/cue/cue.go
+++ backend/cue/cue.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"sync"
 	"time"
 
-	"github.com/nickysemenza/hyperion/backend/color"
 	"github.com/nickysemenza/hyperion/backend/light"
 	log "github.com/sirupsen/logrus"
 )
@@ -29,26 +27,6 @@ func getLogrusFieldsFromContext(ctx context.Context) log.Fields {
 		"cue_id":     ctx.Value(keyCueID),
 		"stack_name": ctx.Value(keyStackName),
 	}
-}
-
-//CM is the cuemaster signleton
-var cueMasterSingleton Master
-
-var once sync.Once
-
-//GetCueMaster makes a singleton for the cue master
-func GetCueMaster() *Master {
-	once.Do(func() {
-		cueMasterSingleton = Master{currentID: 1}
-	})
-	return &cueMasterSingleton
-}
-
-//Master is the parent of all CueStacks, is a singleton
-type Master struct {
-	CueStacks []Stack `json:"cue_stacks"`
-	currentID int64
-	m         sync.Mutex
 }
 
 //Stack is basically a precedence priority queue (really a CueQueue sigh)
@@ -88,57 +66,6 @@ type FrameAction struct {
 	LightName string      `json:"light_name"`
 	//TODO: add `light`
 	//TODO: add way to have a noop action (to block aka wait for time)
-}
-
-//NewFrameAction creates a new instate with incr ID
-func (cm *Master) NewFrameAction(duration time.Duration, color color.RGB, lightName string) FrameAction {
-	return FrameAction{ID: cm.getNextIDForUse(), LightName: lightName, NewState: light.State{RGB: color, Duration: duration}}
-}
-
-//DumpToFile write the CueMaster to a file
-func (cm *Master) DumpToFile(fileName string) error {
-	jsonData, err := json.MarshalIndent(cm, "", " ")
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(fileName, jsonData, 0644)
-
-}
-
-func (cm *Master) getNextIDForUse() int64 {
-	cm.m.Lock()
-	defer cm.m.Unlock()
-
-	id := cm.currentID
-	cm.currentID++
-	return id
-}
-
-//GetDefaultCueStack gives the first cuestack
-func (cm *Master) GetDefaultCueStack() *Stack {
-	return &cm.CueStacks[0]
-}
-
-//NewStack makes a new cue stack
-func (cm *Master) NewStack(priority int, name string) Stack {
-	return Stack{Priority: 2, Name: "main"}
-}
-
-//NewFrame creates a new instate with incr ID
-func (cm *Master) NewFrame(actions []FrameAction) Frame {
-	return Frame{ID: cm.getNextIDForUse(), Actions: actions}
-}
-
-//New creates a new instate with incr ID
-func (cm *Master) New(frames []Frame, name string) Cue {
-	return Cue{ID: cm.getNextIDForUse(), Frames: frames}
-}
-
-//ProcessForever runs all the cuestacks
-func (cm *Master) ProcessForever() {
-	for x := range cm.CueStacks {
-		go cm.CueStacks[x].ProcessStack()
-	}
 }
 
 //ProcessStack processes cues
