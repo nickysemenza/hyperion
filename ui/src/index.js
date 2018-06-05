380e257ff49d83f5ef0077e8769abc844897f1c2
Nicky Semenza <nicky@nickysemenza.com>
2018-06-05 00:33:07 -0700

    load stylesheeds from local FS

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,6 +6,9 @@ import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
+import 'semantic-ui-css/semantic.min.css';
+
 const store = configureStore();
 
 ReactDOM.render(
