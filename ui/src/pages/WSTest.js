a78e05b84ded5cfbdc7eaf3ba039c4e96d2280d2
Nicky Semenza <nicky@nickysemenza.com>
2018-06-20 22:08:40 +0200

    WS status indicator

--- ui/src/pages/WSTest.js
+++ ui/src/pages/WSTest.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import { WS_URL } from '../config';
-import moment from 'moment';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import Sockette from 'sockette';
-import LightTable from '../components/LightTable';
+import React, { Component } from "react";
+import { WS_URL } from "../config";
+import moment from "moment";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import Sockette from "sockette";
+import LightTable from "../components/LightTable";
+import { Label } from "semantic-ui-react";
 
 class WSTest extends Component {
   state = {
     lights: {},
-    curTime: null
+    curTime: null,
+    wsOpen: false
   };
   componentDidMount() {
     setInterval(() => {
       this.setState({
         //   curTime : moment().valueOf()
-        curTime: moment().format('HH:mm:ss:SS (x)')
+        curTime: moment().format("HH:mm:ss:SS (x)")
       });
     }, 10);
 
     const ws = new Sockette(WS_URL, {
-      timeout: 5e3,
-      maxAttempts: 10,
+      // timeout: 5e3,
       onopen: e => {
-        console.log('Connected!', e);
-        ws.send('hi');
+        console.log("Connected!", e);
+        this.setState({ wsOpen: true });
+        ws.send("hi");
       },
       onmessage: e => {
         // console.log('Received:', e);
@@ -33,25 +35,24 @@ class WSTest extends Component {
           this.setState({ lights });
         } catch (error) {}
       },
-      onreconnect: e => console.log('Reconnecting...', e),
-      onmaximum: e => console.log('Stop Attempting!', e),
-      onclose: e => console.log('Closed!', e),
-      onerror: e => console.log('Error:', e)
+      onreconnect: e => console.log("Reconnecting...", e),
+      // onmaximum: e => console.log('Stop Attempting!', e),
+      onclose: e => {
+        console.log("Closed!", e);
+        this.setState({ wsOpen: false });
+      },
+      onerror: e => console.log("Error:", e)
     });
   }
   render() {
-    let { lights } = this.state;
-    // let lightDetail = Object.keys(lights).map((k, i) => {
-    //   let eachLight = lights[k];
-    //   return (
-    //     <LightState key={i} s={eachLight['state']} name={eachLight['name']} />
-    //   );
-    // });
+    let { lights, wsOpen } = this.state;
     return (
       <div>
-        hello
+        <Label color={wsOpen ? "green" : "red"} horizontal>
+          WebSocket
+        </Label>
+        <br />
         <LightTable lights={lights} />
-        {/* {lightDetail} */}
         <pre>{JSON.stringify(this.state.curTime, null, 2)}</pre>
       </div>
     );
